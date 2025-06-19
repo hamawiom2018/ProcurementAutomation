efc1e89d78055ec854390186640fb439ef56b426
hamawiom2018 <42514898+hamawiom2018@users.noreply.github.com>
2025-06-19 16:57:21 +0300

    refactor: adopt minimal hosting

--- Program.cs
+++ Program.cs
@@ -1,46 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using AngularSPAWebAPI.Data;
 using AngularSPAWebAPI.Services;
-using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace AngularSPAWebAPI
+var builder = WebApplication.CreateBuilder(args);
+
+var startup = new AngularSPAWebAPI.Startup(builder.Configuration, builder.Environment);
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
 {
-    public class Program
+    var services = scope.ServiceProvider;
+    try
     {
-        public static void Main(string[] args)
-        {
-            var host = CreateWebHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-
-                try
-                {
-                    var context = services.GetRequiredService<ApplicationDbContext>();
-                    var dbInitializer = services.GetRequiredService<IDbInitializer>();
-                    dbInitializer.Initialize(context).GetAwaiter().GetResult();
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred creating the DB.");
-                }
-            }
-
-            host.Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        var dbInitializer = services.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize(context).GetAwaiter().GetResult();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred creating the DB.");
     }
 }
+
+startup.Configure(app, builder.Environment);
+
+app.Run();
