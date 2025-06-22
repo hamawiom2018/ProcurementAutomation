04ae633313b8db572494a6da70371d1b491385cb
Omar Alhamawi <oalhamawi@seec.gov.sa>
2025-06-22 09:21:09 +0300

    remove all

--- Models/AccountViewModels/LoginViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace AngularSPAWebAPI.Models.AccountViewModels
-{
-    public class LoginViewModel
-    {
-        public string username { get; set; }
-        public string password { get; set; }
-    }
-}
--- Models/AdOptions.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace AngularSPAWebAPI.Models
-{
-    public class AdOptions
-    {
-        public string Domain { get; set; }
-    }
-}
